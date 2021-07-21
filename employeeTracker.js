58f5af86c8735ace37aad25aecb1c93f9ed85200
Israel Magallon <browncans@gmail.com>
2021-07-20 19:51:36 -0700

    working runEmployee function to start prompt with switch

--- employeeTracker.js
+++ employeeTracker.js
@@ -17,5 +17,63 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
   if (err) throw err;
- 
+  runEmployee();
 });
+
+const runEmployee = () => {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Employees by Department",
+        "View All Employees by Roles",
+        "Add Department",
+        "Add Role",
+        "Add Employee",
+        "Update Employee",
+        "Exit",
+      ],
+    })
+    .then((answer) => {
+      switch (answer.action) {
+        case "View All Employees":
+          viewEmployees();
+          break;
+
+        case "View All Employees by Department":
+          viewDepartments();
+          break;
+
+        case "View All Employees by Roles":
+          viewRoles();
+          break;
+
+        case "Add Department":
+          addDepartment();
+          break;
+
+        case "Add Role":
+          addRole();
+          break;
+
+        case "Add Employee":
+          addEmployee();
+          break;
+
+        case "Update Employee":
+          updateEmployee();
+          break;
+
+        case "Exit":
+          connection.end();
+          break;
+
+        default:
+          console.log(`Invalid action: ${answer.action}`);
+          break;
+      }
+    });
+};
